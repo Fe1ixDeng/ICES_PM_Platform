9ce0a1aae223a4338817f497b93dfdea23490e87
Fe1ixDeng <92066832+Fe1ixDeng@users.noreply.github.com>
2023-01-02 00:16:26 +0800

    完善前四个实验框架：alpha、hm、im、vspm

--- src/main/java/org/hit/ices/ices_pm_platform/controller/HTMLController.java
+++ src/main/java/org/hit/ices/ices_pm_platform/controller/HTMLController.java
@@ -17,15 +17,95 @@ public class HTMLController {
     }
 //    临时引用页面，在建功能可以引用
     @RequestMapping("/ices_pm_platform_tempReference.html")
-    public String index_tempReference(){
-    return "ices_pm_platform_tempReference";
+    public String index_tempReference(){return "ices_pm_platform_tempReference";}
+
+/*----以下是实验部分----------------------------------------------------------------------------------------------------*/
+
+//    alpha算法门户
+    @RequestMapping("/ices_pm_alphaalgorithm/ices_pm_alphaalgorithm_portal.html")
+    public String index_alphaalgorithm(){
+    return "ices_pm_alphaalgorithm/ices_pm_alphaalgorithm_portal";
 }
-//   vspm门户
+//    alpha算法展示页面
+    @RequestMapping("/ices_pm_alphaalgorithm/ices_pm_alphaalgorithm_default.html")
+    public String index_alphaalgorithm_default(){return "ices_pm_alphaalgorithm/ices_pm_alphaalgorithm_default";}
+    @RequestMapping("/ices_pm_alphaalgorithm/ices_pm_alphaalgorithm_logInfo.html")
+    public String index_alphaalgorithm_logInfo(){
+        return "ices_pm_alphaalgorithm/ices_pm_alphaalgorithm_logInfo";
+    }
+    @RequestMapping("/ices_pm_alphaalgorithm/ices_pm_alphaalgorithm_instanceInfo.html")
+    public String index_alphaalgorithm_instanceInfo(){return "ices_pm_alphaalgorithm/ices_pm_alphaalgorithm_instanceInfo";}
+    @RequestMapping("/ices_pm_alphaalgorithm/ices_pm_alphaalgorithm_variantInfo.html")
+    public String index_alphaalgorithm_variantInfo(){
+        return "ices_pm_alphaalgorithm/ices_pm_alphaalgorithm_variantInfo";
+    }
+    @RequestMapping("/ices_pm_alphaalgorithm/ices_pm_alphaalgorithm_variantGraph.html")
+    public String index_alphaalgorithm_variantGraph(){
+        return "ices_pm_alphaalgorithm/ices_pm_alphaalgorithm_variantGraph";
+    }
+    @RequestMapping("/ices_pm_alphaalgorithm/ices_pm_alphaalgorithm_processModel.html")
+    public String index_alphaalgorithm_processModel(){return "ices_pm_alphaalgorithm/ices_pm_alphaalgorithm_processModel";}
+
+//    hm门户
+    @RequestMapping("/ices_pm_hm/ices_pm_hm_portal.html")
+    public String index_hm(){
+    return "ices_pm_hm/ices_pm_hm_portal";
+}
+//    hm展示页面
+    @RequestMapping("/ices_pm_hm/ices_pm_hm_default.html")
+    public String index_hm_default(){return "ices_pm_hm/ices_pm_hm_default";}
+    @RequestMapping("/ices_pm_hm/ices_pm_hm_logInfo.html")
+    public String index_hm_logInfo(){
+        return "ices_pm_hm/ices_pm_hm_logInfo";
+    }
+    @RequestMapping("/ices_pm_hm/ices_pm_hm_instanceInfo.html")
+    public String index_hm_instanceInfo(){return "ices_pm_hm/ices_pm_hm_instanceInfo";}
+    @RequestMapping("/ices_pm_hm/ices_pm_hm_variantInfo.html")
+    public String index_hm_variantInfo(){
+        return "ices_pm_hm/ices_pm_hm_variantInfo";
+    }
+    @RequestMapping("/ices_pm_hm/ices_pm_hm_variantGraph.html")
+    public String index_hm_variantGraph(){
+        return "ices_pm_hm/ices_pm_hm_variantGraph";
+    }
+    @RequestMapping("/ices_pm_hm/ices_pm_hm_processModel.html")
+    public String index_hm_processModel(){
+        return "ices_pm_hm/ices_pm_hm_processModel";
+    }
+
+//    im门户
+    @RequestMapping("/ices_pm_im/ices_pm_im_portal.html")
+    public String index_im(){
+        return "ices_pm_im/ices_pm_im_portal";
+    }
+//    im展示页面
+    @RequestMapping("/ices_pm_im/ices_pm_im_default.html")
+    public String index_im_default(){return "ices_pm_im/ices_pm_im_default";}
+    @RequestMapping("/ices_pm_im/ices_pm_im_logInfo.html")
+    public String index_im_logInfo(){
+        return "ices_pm_im/ices_pm_im_logInfo";
+    }
+    @RequestMapping("/ices_pm_im/ices_pm_im_instanceInfo.html")
+    public String index_im_instanceInfo(){return "ices_pm_im/ices_pm_im_instanceInfo";}
+    @RequestMapping("/ices_pm_im/ices_pm_im_variantInfo.html")
+    public String index_im_variantInfo(){
+        return "ices_pm_im/ices_pm_im_variantInfo";
+    }
+    @RequestMapping("/ices_pm_im/ices_pm_im_variantGraph.html")
+    public String index_im_variantGraph(){
+        return "ices_pm_im/ices_pm_im_variantGraph";
+    }
+    @RequestMapping("/ices_pm_im/ices_pm_im_processModel.html")
+    public String index_im_processModel(){
+        return "ices_pm_im/ices_pm_im_processModel";
+    }
+
+//    vspm门户
     @RequestMapping("/ices_pm_vspm/ices_pm_vspm_portal.html")
     public String index_vspm(){
         return "ices_pm_vspm/ices_pm_vspm_portal";
     }
-//  vspm展示页面
+//    vspm展示页面
     @RequestMapping("/ices_pm_vspm/ices_pm_vspm_default.html")
     public String index_vspm_default(){
         return "ices_pm_vspm/ices_pm_vspm_default";
---
+++ src/main/resources/static/js/js-portal.js
@@ -0,0 +1,19 @@
+function setSubmit() {
+    switch (document.getElementById("algo").value){
+        case "select":
+            window.alert("Please choose an Algorithm!");
+            break;
+        case "aAlgorithm":
+            document.getElementById("defaultDisplay").src = "ices_pm_alphaalgorithm/ices_pm_alphaalgorithm_portal.html";
+            break;
+        case "HeuristicMining":
+            document.getElementById("defaultDisplay").src = "ices_pm_hm/ices_pm_hm_portal.html";
+            break;
+        case "InductiveMining":
+            document.getElementById("defaultDisplay").src = "ices_pm_im/ices_pm_im_portal.html";
+            break;
+        case "VSPM":
+            document.getElementById("defaultDisplay").src = "ices_pm_vspm/ices_pm_vspm_portal.html";
+            break;
+    }
+}
